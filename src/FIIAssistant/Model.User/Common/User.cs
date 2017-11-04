6dbba0023706909d8b373cece4e88993e91d3464
Dan N <dan.nastasa20@gmail.com>
2017-11-04 13:45:54 +0200

    Added Model.User project

---
+++ src/FIIAssistant/Model.User/Common/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Commons;
+
+namespace Model.User.Common {
+    public abstract class User : Entity {
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
