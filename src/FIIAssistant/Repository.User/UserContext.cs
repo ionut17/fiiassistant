0fbf7e2a6246eb4c7e3c252bdf64ab8225735046
Dan N <dan.nastasa20@gmail.com>
2017-11-04 12:34:54 +0200

    User Component

---
+++ src/FIIAssistant/Repository.User/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.User.Entities;
+
+namespace Repository.User {
+    public class UserContext : DbContext {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Authentication> Authentications { get; set; }
+    }
+}
---
+++ src/FIIAssistant/Service.User/UserService.cs
@@ -0,0 +1,11 @@
+﻿using Repository.User;
+
+namespace Service.User {
+    public class UserService {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository) {
+            _userRepository = userRepository;
+        }
+    }
+}
