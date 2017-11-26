b27aafd56d92ad3d97dfe0e2204d87aa819c050d
gordins <stefangordindev@gmail.com>
2017-11-26 11:46:36 +0200

    Resolved merge errors

--- src/FIIAssistant/IntegrationTests/Server/GivenStudentsApiController.cs
+++ src/FIIAssistant/IntegrationTests/Server/GivenStudentsApiController.cs
@@ -2,7 +2,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Server.Controllers;
 using User.Data.Model.Entities;
-using User.Presentation.Controllers;
 
 namespace IntegrationTests.Server
 {
@@ -17,18 +16,18 @@ namespace IntegrationTests.Server
             Sut = new StudentsApiController();
         }
 
-        [TestMethod]
-        [Ignore]
-        //It is normal to fail, the application is not in debug mode nor hosted
-        public void When_PostingAStudent_Then_CountShouldIncrease()
-        {
-            //arrange
-            var expectedCount = StudentsController.Students.Count + 1;
-            //act
-            var result = Sut.Post(new Student("Stefan"));
-            //assert
-            StudentsController.Students.Count.Should().Be(expectedCount);
-        }
+//        [TestMethod]
+//        [Ignore]
+//        //It is normal to fail, the application is not in debug mode nor hosted
+//        public void When_PostingAStudent_Then_CountShouldIncrease()
+//        {
+//            //arrange
+//            var expectedCount = StudentsController.Students.Count + 1;
+//            //act
+//            var result = Sut.Post(new Student("Stefan"));
+//            //assert
+//            StudentsController.Students.Count.Should().Be(expectedCount);
+//        }
 
         [ClassCleanup]
         public static void Cleanup()
